5fcb7d96bd6cc90370e4625a304c25e0a31af2d6
Raymond Doetjes <rdoetjes@phonax.com>
2022-10-18 16:11:32 +0200

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::{process::{Command, Stdio}, io::Read};
+
+fn get_tags_as_string() -> String {
+    let process = match Command::new("git")
+    .args(["tag"])
+    .stdout(Stdio::piped())
+    .spawn() {
+        Err(why) => {
+            eprintln!("couldn't spawn git tag: {}", why);
+            std::process::exit(1);
+        },
+        Ok(process) => process,
+    };
+    
+    let mut s = String::new();
+    match process.stdout.unwrap().read_to_string(&mut s){
+        Ok(_) => {},
+        Err(e) => {
+            eprintln!("Error trying to read from git tag : {}", e);
+            std::process::exit(1);
+        },
+    };
+    s
+}
+
+fn turn_tags_into_number(tags: String){
+    let mut tags_sorted = Vec::new();
+    for tag in tags.split("\n"){
+        tags_sorted.push(tag)
+    }
+    
+    tags_sorted.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
+    println!("{:?}", tags_sorted);
+}
+
 fn main() {
-    println!("Hello, world!");
+    turn_tags_into_number(get_tags_as_string());
+    
 }
