2aa2dcb1d62f0e42106113cb34611be2a962346c
Raymond Doetjes <rdoetjes@phonax.com>
2022-10-18 17:33:56 +0200

    comments

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,16 @@ use std::{process::{Command, Stdio}, io::Read, str::FromStr};
 
 use regex::Regex;
 
+/// get_tags_as_string returns a string with all the tags from the central repository
+/// 
+/// Generates a string by issuing the following command: 
+/// git ls-remote --tags --sort=committerdate and reading the output
+/// via a pipe into a variable.
+/// The --sort=comtterdate is very important because that's the only way to determine the last tag
+/// 
+/// Example:
+/// println!("{}", get_tags_as_string());
+/// 
 fn get_tags_as_string() -> String {
     let process = match Command::new("git")
     .args(["ls-remote", "--tags", "--sort=committerdate"])
@@ -25,6 +35,11 @@ fn get_tags_as_string() -> String {
     s
 }
 
+///get_last_tag(tags) returns the last tag
+/// 
+/// It gets the last summited tag fbe 
+/// This gets the last tag based on the last entry that is created in the tags_sorted vector
+/// then running a regular expression to only collect the tag name (at the end of the line using regex /*/(.*)/)
 fn get_last_tag(tags: String) -> String {
     let mut tags_sorted = Vec::new();
     for tag in tags.split("\n"){
@@ -45,6 +60,10 @@ fn get_last_tag(tags: String) -> String {
     last_tag.to_string()
 }
 
+/// next_tag(tag) creates the last minor number of the 1.0.1 string returning 1.0.2
+/// 
+/// next_tag(tag) parses the x.y.z tag and cover z to i32 adds 1 and returns the string
+/// x.y.z+1
 fn next_tag(tag: String) -> String {
     let vec: Vec<&str> = tag.split(".").collect();
     let mut c: i32 = FromStr::from_str(vec[2]).unwrap();
